c032d78fc1f02e10b24c0bbbd9e06c4aa1cb189a
regis <regis@LAPTOP-MO8U2HLG>
2022-12-11 00:53:17 +0000

    init

---
+++ Example.Benchmark/StringConcatBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+public class StringConcatBenchmark
+{
+    private const int N = 10000;
+
+    [Benchmark]
+    public string Concat()
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < N; i++)
+        {
+            sb.Append("Hello ");
+            sb.Append("World");
+        }
+        return sb.ToString();
+    }
+
+    [Benchmark]
+    public string Interpolation()
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < N; i++)
+        {
+            sb.Append($"Hello {i}");
+        }
+        return sb.ToString();
+    }
+}
